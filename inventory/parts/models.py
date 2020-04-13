8260a88270c967111126b2c9336c7d42a3aaf1a5
admin <fahey32@gmail.com>
2020-04-14 06:37:41 +1000

    HTML template working

--- inventory/parts/models.py
+++ inventory/parts/models.py
@@ -6,13 +6,13 @@ class partslist(models.Model):
     description = models.CharField(max_length=200,)
     location = models.CharField(max_length=20)
     supplier = models.CharField(max_length=50)
-    stockonhand = models.IntegerField()
-    minimumstock = models.IntegerField()
-    reorderqtys = models.IntegerField()
-    qtyperassembly = models.IntegerField()
-    boxsize = models.IntegerField()
+    stockonhand = models.IntegerField(blank=True)
+    minimumstock = models.IntegerField(blank=True)
+    reorderqtys = models.IntegerField(blank=True)
+    qtyperassembly = models.IntegerField(blank=True)
+    boxsize = models.IntegerField(blank=True)
     leadtime = models.CharField(max_length=50)
-    weight = models.IntegerField()
+    weight = models.IntegerField(blank=True)
 
 # dunder used to return string value without conflicting with other variables
     def __str__(self):
