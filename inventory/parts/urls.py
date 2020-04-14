63abbe91b4fc49f7a941ca4699f38cef86b41f4d
admin <fahey32@gmail.com>
2020-04-15 07:15:37 +1000

    HTML template working

--- inventory/parts/urls.py
+++ inventory/parts/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('inventory/', inventory_list, name='inventory'), # URL path for inventory_list view
+#    path('detail/', detail_view, name='detail'),
     ]
--- inventory/parts/views.py
+++ inventory/parts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 
 # Create your views here.
@@ -9,3 +9,17 @@ def inventory_list(request):
         'header': 'Inventory'
     }
     return render(request, 'inventory.html', context)
+
+def detail_view(request, pk, model, cls):
+    item = get_object_or_404(model, pk=pk)
+
+    if request.method == "POST":
+        form = cls(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('inventory')
+
+    else:
+        form = cls(instance=item)
+
+        return render(request, 'detail.html', {'form': form})
